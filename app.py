9e6938ffbbd1c32dd72728945b8de304ef3ddc0d
THINGUYEN\vicky <tnguyen143@ivytech.edu>
2024-04-13 21:06:20 -0400

    create book route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -7,3 +7,11 @@ app = Flask(__name__)
 @app.route('/book/<int:book_id>')
 def read_book_by_id(book_id):
     return f"book id is: {book_id}"
+
+
+@app.route('/book', methods=['POST'])
+def create_book():
+    book_name = request.json.get('book_name')
+    author = request.json.get('author')
+    publisher = request.json.get('publisher')
+    return f"book name: {book_name}, author: {author}, publisher: {publisher}"
