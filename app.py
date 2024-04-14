deb6f77a2ba1433e86eb29eed814103aed62f398
THINGUYEN\vicky <tnguyen143@ivytech.edu>
2024-04-14 11:28:21 -0400

    integrate with sqlalchemy and make read book api and create book api

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_name = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    publisher = db.Column(db.String(100), nullable=False)
+
+    def to_dict(self):
+        return dict(id=self.id, book_name=self.book_name, author=self.author, publisher=self.publisher)
+
 
 # Create routes
 
 @app.route('/book/<int:book_id>')
 def read_book_by_id(book_id):
-    return f"book id is: {book_id}"
+    book = db.get_or_404(Book, book_id)
+    return book.to_dict()
 
 
 @app.route('/book', methods=['POST'])
@@ -14,14 +35,19 @@ def create_book():
     book_name = request.json.get('book_name')
     author = request.json.get('author')
     publisher = request.json.get('publisher')
-    return f"book name: {book_name}, author: {author}, publisher: {publisher}"
+
+    book = Book(book_name=book_name, author=author, publisher=publisher)
+    db.session.add(book)
+    db.session.commit()
+
+    return book.to_dict()
 
 
-@app.route('book/<int:book_id>', methods=["PUT", "PATCH"])
+@app.route('/book/<int:book_id>', methods=["PUT", "PATCH"])
 def update_book(book_id):
     return f"book id is: {book_id}"
 
 
-@app.route('book/<int:book_id>', methods=["DELETE"])
+@app.route('/book/<int:book_id>', methods=["DELETE"])
 def delete_book(book_id):
     return f"book id is: {book_id}"
