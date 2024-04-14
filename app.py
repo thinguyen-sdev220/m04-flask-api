f9940053216165a69856326bb93b513178dc5938
THINGUYEN\vicky <tnguyen143@ivytech.edu>
2024-04-14 11:51:52 -0400

    Add header and line comments

--- app.py
+++ app.py
@@ -1,35 +1,44 @@
+"""
+File: app.py
+Author: Thi Nguyen
+Description: Create a CRUD API for a Book
+"""
+
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 
 app = Flask(__name__)
 
+# Database configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 class Base(DeclarativeBase):
   pass
 
+# Initialize SQLAlchemy with Flask app
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
-
+# Book Model
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     book_name = db.Column(db.String(100), nullable=False)
     author = db.Column(db.String(100), nullable=False)
     publisher = db.Column(db.String(100), nullable=False)
-
+    # Converts the Book object to a dictionary
     def to_dict(self):
         return dict(id=self.id, book_name=self.book_name, author=self.author, publisher=self.publisher)
 
 
 # Create routes
 
+# Route to read a book by its ID
 @app.route('/book/<int:book_id>')
 def read_book_by_id(book_id):
     book = db.get_or_404(Book, book_id)
     return book.to_dict()
 
-
+# Route to create a new book
 @app.route('/book', methods=['POST'])
 def create_book():
     book_name = request.json.get('book_name')
@@ -42,7 +51,7 @@ def create_book():
 
     return book.to_dict()
 
-
+# Route to update a book
 @app.route('/book/<int:book_id>', methods=["PUT", "PATCH"])
 def update_book(book_id):
     book = db.get_or_404(Book, book_id)
@@ -63,7 +72,7 @@ def update_book(book_id):
 
     return book.to_dict()
 
-
+# Route to delete a book
 @app.route('/book/<int:book_id>', methods=["DELETE"])
 def delete_book(book_id):
     book = db.get_or_404(Book, book_id)
